cf74e52de027f0d9a6fe45b0668dde46a3f6bedb
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-04-27 13:22:36 -0500

    Create i3.sh

---
+++ deploy/i3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path" # Ensures correct directory
+cd ../src
+ls
+rm -rf ~/.config/i3
+rm -rf ~/.i3
+rm -rf ~/.config/i3status
+rm -rf ~/.config/i3blocks
+
+cp -r i3/ ~/.config/
+i3-msg reload
+i3-msg restart
