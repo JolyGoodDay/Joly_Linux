3d433b77f17ef1deeb52964bf1bd2fe8743fc8f7
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-09-11 18:23:00 -0500

    Create multiline_string.py

---
+++ scripts/multiline_string.py
@@ -0,0 +1,20 @@
+print("foo bar")
+print("-"*10)
+
+print("foo "
+      "bar")
+print("-"*10)
+
+print("foo "+
+      "bar")
+print("-"*10)
+
+print("foo \
+      bar")
+print("-"*10)
+
+print("foo \
+      \nbar")
+foo = "foo"
+bar = "bar"
+
