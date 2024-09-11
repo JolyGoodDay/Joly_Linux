48da8ea6b7cb8bfc9b55667754e5a7075b167462
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-09-11 18:19:05 -0500

    Create setup.sh

---
+++ src/i3/setup.sh
@@ -0,0 +1,13 @@
+#GTK Dark Mode
+
+./usr/lib/gsd-xsettings
+# Numlock
+./usr/bin/numlockx on
+# Fix screens
+xrandr --screen 0 --output DP-6.1.8 --auto --right-of DP-6.8
+# exec_always xrandr --output DP-1 --scale 1x1
+# exec_always xrandr --dpi 196
+
+
+
+# todo set font to font pango:monospace 12 on ubuntu
