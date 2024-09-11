0c818b7c170f11d56135056542a027320b699bee
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-09-11 18:15:02 -0500

    Update i3.sh

--- deploy/i3.sh
+++ deploy/i3.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source "$(dirname "${BASH_SOURCE[0]}")/../src/common/set_joly_linux"
 
-cd "$parent_path" # Ensures correct directory
-cd ../src
-ls
+
 rm -rf ~/.config/i3
 rm -rf ~/.i3
 rm -rf ~/.config/i3status
 rm -rf ~/.config/i3blocks
 
-cp -r i3/ ~/.config/
+cp -r src/i3/ ~/.config/
 i3-msg reload
 i3-msg restart
