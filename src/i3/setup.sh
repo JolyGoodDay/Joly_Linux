81d0e51a0684b12fd2520169eb05cf94dc8ca27a
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-09-12 21:57:52 -0500

    darkmode fixes

--- src/i3/setup.sh
+++ src/i3/setup.sh
@@ -3,7 +3,46 @@ os_name=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
 config_file="$HOME/.config/i3/config"
 #GTK Dark Mode
 
-./usr/lib/gsd-xsettings
+# Ensure GTK configuration directory and file exist
+mkdir -p ~/.config/gtk-3.0
+if [ ! -f ~/.config/gtk-3.0/settings.ini ]; then
+    echo '[Settings]' > ~/.config/gtk-3.0/settings.ini
+fi
+if ! grep -q 'gtk-theme-name = Adwaita-dark' ~/.config/gtk-3.0/settings.ini; then
+    echo 'gtk-theme-name = Adwaita-dark' >> ~/.config/gtk-3.0/settings.ini
+fi
+
+# Ensure QT configuration directory and file exist
+mkdir -p ~/.config/qt5ct
+if [ ! -f ~/.config/qt5ct/qt5ct.conf ]; then
+    echo '[Appearance]' > ~/.config/qt5ct/qt5ct.conf
+fi
+if ! grep -q 'style=gtk2' ~/.config/qt5ct/qt5ct.conf; then
+    echo 'style=gtk2' >> ~/.config/qt5ct/qt5ct.conf
+    echo 'palette=qt5ct' >> ~/.config/qt5ct/qt5ct.conf
+fi
+
+# Ensure .Xresources file exists and add URxvt theme if not already present
+if [ ! -f ~/.Xresources ]; then
+    touch ~/.Xresources
+fi
+if ! grep -q 'URxvt.background: black' ~/.Xresources; then
+    echo 'URxvt.background: black' >> ~/.Xresources
+    echo 'URxvt.foreground: white' >> ~/.Xresources
+    xrdb ~/.Xresources
+fi
+
+# Ensure Alacritty configuration directory and file exist
+mkdir -p ~/.config/alacritty
+if [ ! -f ~/.config/alacritty/alacritty.yml ]; then
+    touch ~/.config/alacritty/alacritty.yml
+fi
+if ! grep -q 'background: "0x1d1f21"' ~/.config/alacritty/alacritty.yml; then
+    echo 'colors:' > ~/.config/alacritty/alacritty.yml
+    echo '  primary:' >> ~/.config/alacritty/alacritty.yml
+    echo '    background: "0x1d1f21"' >> ~/.config/alacritty/alacritty.yml
+    echo '    foreground: "0xc5c8c6"' >> ~/.config/alacritty/alacritty.yml
+fi
 # Numlock
 ./usr/bin/numlockx on
 
---
+++ src/i3/weather.sh
@@ -0,0 +1,1 @@
+curl -Ss 'https://wttr.in/78602?format=%C+%t+%w' | xargs echo
