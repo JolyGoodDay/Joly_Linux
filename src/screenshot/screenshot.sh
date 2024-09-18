6d5486e3f74d11a12761dfd24c0242c368e1426c
JolyGoodDay <7170917+JolyGoodDay@users.noreply.github.com>
2024-09-18 14:33:30 -0500

    Update screenshot.sh

--- src/screenshot/screenshot.sh
+++ src/screenshot/screenshot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path" # Ensures correct directory
-source deactivate 2> /dev/null
+echo $parent_path
+gcc -shared -O3 -lX11 -fPIC -Wl,-soname,prtscn -o prtscn.so prtscn.c
+python3.7 -m venv venv
 source venv/bin/activate
-python src/screenshot/screenshot.py
+pip install pillow xlib
+python screenshot.py
+
+#https://stackoverflow.com/questions/69645/take-a-screenshot-via-a-python-script-on-linux
